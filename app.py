c19528fb445c911ede6485ba197b33424421259e
hsienyia <hsienyia@gmail.com>
2026-05-24 01:44:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -346,13 +346,13 @@ def calculate_single_board_score(html_content, mode):
         has_jie_hard = "地劫" in p_text
             
         if has_kong_hard and has_jie_hard:
-            score -= 50
+            score -= 70
             process_log.append(f"❌ `{p_name}` 地空地劫同宮重傷 (-100分)")
         elif has_kong_hard:
-            score -= 20
+            score -= 30
             process_log.append(f"❌ `{p_name}` 逢地空星 (-40分)")
         elif has_jie_hard:
-            score -= 20
+            score -= 30
             process_log.append(f"❌ `{p_name}` 逢地劫星 (-40分)")
         elif has_kong_soft:
             score -= 10
